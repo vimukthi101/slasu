b4a821ddf2fb74742018ccf466853040c0db50e0
vimukthi101 <vimukthisaranga1@gmail.com>
2021-10-06 13:41:48 +0530

    bug fix

--- adminPanel/adminPages/exportToPdf-allAdmin.php
+++ adminPanel/adminPages/exportToPdf-allAdmin.php
@@ -1,9 +1,18 @@
 <?php
+namespace PhpOffice;
+include ".\PhpOffice\autoload.php";
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Color;
+
 if(!isset($_SESSION[''])){
     session_start();
 }
+
 include_once('../../php/db.php');
 include_once('../dist/print.php');
+
 if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
 	if(!empty($_POST['regRadio']) && isset($_POST['fieldList']) && !empty($_POST['fieldList'])){
 		$reportType = $_POST['regRadio'];
@@ -26,14 +35,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
 				$html .= '</tr>';
 			}
 			$html .= '</table>';
-			if($reportType == "pdf"){
+			if($reportType == "excel"){
+				// header('Location:excel.php?file=all-admin-report&html='.$html);
+				$fileName = "all-admin-report";
+				$CntDisposition = "Content-Disposition: attachment;filename=";
+				$CntDisposition = $CntDisposition . $fileName . ".xls";
+				header($CntDisposition);
+				header('Cache-Control: max-age=0');
+				$reader = new \PhpOffice\PhpSpreadsheet\Reader\Html();
+				$htmlString = $html;
+				$spreadsheet = $reader->loadFromString($htmlString);
+				$writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xls');
+				$writer->save('php://output');
+			} else if($reportType == "pdf") {
 				$pdf->AddPage('L', 'A4');
 				$pdf->Cell(0, 0, 'View All Admin Details', 1, 1, 'C');
 				$pdf->writeHTML($html, true, false, true, false, '');
 				$pdf->lastPage();
 				$pdf->Output('all-admin-report.pdf', 'D');
-			} else {
-				header('Location:excel.php?file=all-admin-report&html='.$html);
 			}
 		}
 	} else {
