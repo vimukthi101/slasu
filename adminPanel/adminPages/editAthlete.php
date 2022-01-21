fe5d87edf95cbe3f13b6082b05ca0fcc0c11b593
Vimukthi Saranga <vsaranga@wiley.com>
2022-01-21 08:38:50 +0530

    update club of athlete

--- adminPanel/adminPages/editAthlete.php
+++ adminPanel/adminPages/editAthlete.php
@@ -306,7 +306,7 @@
                                     while($rowClub = mysqli_fetch_array($resultClub)){
                                         $newClubId = $rowClub['clubId'];
                                         $newClubName = $rowClub['clubName'];
-                                        echo '<option selected value="'.$clubId.'">'.$clubName.'</option>';
+                                        echo '<option selected value="'.$newClubId.'">'.$newClubName.'</option>';
                                     }
                                 }
                               echo '</select>
